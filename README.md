2d5930e6f8379d57179cc95d5aa341ad959d07d6
Romina <riedelromina@gmail.com>
2020-01-29 17:37:31 -0300

    Initial commit with base React App and Express Back

---
+++ README.md
@@ -0,0 +1,3 @@
+## Acc Statistics
+
+Base app with React App code located at acc-statistics-front and Express back located at acc-statistics-back.
---
+++ acc-statistics-back/routes/statistics.js
@@ -0,0 +1,129 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET statistics listing. */
+router.get("/", function(req, res, next) {
+  res.json(data);
+});
+
+const data = [
+  {
+    username: "andrada",
+    name: "Andrada",
+    acc6: 112,
+    "acc6%": 5.8,
+    acc7: 13,
+    "acc7%": 0.7,
+    acc8: 0,
+    "acc8%": 9.32,
+    bar1: 11,
+    bar2: 33,
+    bar3: 9
+  },
+  {
+    username: "mas",
+    name: "M·s",
+    acc6: 112,
+    "acc6%": 5.8,
+    acc7: 13,
+    "acc7%": 0.7,
+    acc8: 0,
+    "acc8%": 9.32,
+    bar1: 11,
+    bar2: 33,
+    bar3: 9
+  },
+  {
+    username: "buffarini",
+    name: "Buffarini",
+    acc6: 112,
+    "acc6%": 5.8,
+    acc7: 13,
+    "acc7%": 0.7,
+    acc8: 0,
+    "acc8%": 9.32,
+    bar1: 11,
+    bar2: 33,
+    bar3: 9
+  },
+  {
+    username: "lopez",
+    name: "Lopez",
+    acc6: 112,
+    "acc6%": 5.8,
+    acc7: 13,
+    "acc7%": 0.7,
+    acc8: 0,
+    "acc8%": 9.32,
+    bar1: 11,
+    bar2: 33,
+    bar3: 9
+  },
+  {
+    username: "izquierdoz",
+    name: "Izquierdoz",
+    acc6: 112,
+    "acc6%": 5.8,
+    acc7: 13,
+    "acc7%": 0.7,
+    acc8: 0,
+    "acc8%": 9.32,
+    bar1: 11,
+    bar2: 33,
+    bar3: 9
+  },
+  {
+    username: "Marcone",
+    name: "Marcone",
+    acc6: 112,
+    "acc6%": 5.8,
+    acc7: 13,
+    "acc7%": 0.7,
+    acc8: 0,
+    "acc8%": 9.32,
+    bar1: 11,
+    bar2: 33,
+    bar3: 9
+  },
+  {
+    username: "Benedetto",
+    name: "Benedetto",
+    acc6: 112,
+    "acc6%": 5.8,
+    acc7: 13,
+    "acc7%": 0.7,
+    acc8: 0,
+    "acc8%": 9.32,
+    bar1: 11,
+    bar2: 33,
+    bar3: 9
+  },
+  {
+    username: "Nandez",
+    name: "Nandez",
+    acc6: 112,
+    "acc6%": 5.8,
+    acc7: 13,
+    "acc7%": 0.7,
+    acc8: 0,
+    "acc8%": 9.32,
+    bar1: 11,
+    bar2: 33,
+    bar3: 9
+  },
+  {
+    username: "Campuzano",
+    name: "Campuzano",
+    acc6: 112,
+    "acc6%": 5.8,
+    acc7: 13,
+    "acc7%": 0.7,
+    acc8: 0,
+    "acc8%": 9.32,
+    bar1: 11,
+    bar2: 33,
+    bar3: 9
+  }
+];
+
+module.exports = router;
